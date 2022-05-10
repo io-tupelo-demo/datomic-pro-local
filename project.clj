b260a606eaa6afd822aa248b2c20fe8d9ee7b5ad
Alan Thompson <alan.thompson@railsbank.com>
2022-05-09 23:57:12 -0700

    misc

--- project.clj
+++ project.clj
@@ -1,22 +1,20 @@
-(defproject io-tupelo-demo/datomic-basic "0.9.4"
+(defproject io-tupelo-demo/datomic-pro-local "0.1.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [com.datomic/datomic-pro "1.0.6269"]
-                 [org.clojure/clojure "1.10.0"]
-                 [prismatic/schema "1.1.10"]
+                 [org.clojure/clojure "1.11.1"]
+                 [prismatic/schema "1.2.1"]
                  [tupelo "22.05.04"]
                  ]
   :resource-paths ["resources/"]
 
   :global-vars { *warn-on-reflection* false }
 
-  :plugins  [ [lein-codox "0.10.3"] ]
   :update :daily ;  :always
 
   :target-path "target/%s"
   :clean-targets [ "target" ]
 
-  :jvm-opts ["-Xms500m" "-Xmx2g"
-            ]
+  :jvm-opts ["-Xms500m" "-Xmx2g"]
 )
